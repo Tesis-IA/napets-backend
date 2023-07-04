f1d537a877c278d5ef726a37a2690d9b09863545
Francisco Garcia <jjjchico1@gmail.com>
2023-07-04 17:29:08 -0600

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './modules/users/users.module'
-import { AuthModule } from './modules/auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module'
 
 @Module({
   imports: [
@@ -21,10 +21,11 @@ import { AuthModule } from './modules/auth/auth.module';
       username: process.env.POSTGRES_USER,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
-      ssl: {      /* <----- Add SSL option */
-        require: true,
-        rejectUnauthorized: false
-      }
+      ssl: {
+        /* <----- Add SSL option */
+        requestCert: true,
+        rejectUnauthorized: false,
+      },
     }),
     UsersModule,
     AuthModule,
