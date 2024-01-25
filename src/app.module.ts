e3817527f751d76178ea67266e069eb3e097a7a3
MJackson22-bit <jm5444859@gmail.com>
2024-01-24 22:07:46 -0600

    Add missing module to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,46 +1,50 @@
-import {Module} from '@nestjs/common'
-import {ConfigModule} from '@nestjs/config'
-import {TypeOrmModule} from '@nestjs/typeorm'
-import {AppController} from './app.controller'
-import {AppService} from './app.service'
-import {UsersModule} from './modules/users/users.module'
-import {AuthModule} from './modules/auth/auth.module'
-import {SubjectsModule} from "./modules/home/subjects/subjects.module";
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UsersModule } from './modules/users/users.module'
+import { AuthModule } from './modules/auth/auth.module'
+import { SubjectsModule } from './modules/home/subjects/subjects.module'
 import { join } from 'path'
 import { PredictionModule } from './modules/prediction/prediction.module'
 import { ServeStaticModule } from '@nestjs/serve-static'
+import { PestDiseaseModule } from './modules/pest-disease/pest-disease.module'
+import { CropsTipsModule } from './modules/crops-tips/crops-tips.module'
 
 @Module({
-    imports: [
-        ServeStaticModule.forRoot({
-            rootPath: join(__dirname, '..')
-        }),
-        ConfigModule.forRoot({
-            envFilePath: '.env',
-            isGlobal: true,
-        }),
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            host: process.env.POSTGRES_HOST,
-            database: process.env.POSTGRES_DB,
-            port: parseInt(process.env.POSTGRES_PORT),
-            password: process.env.POSTGRES_PASSWORD,
-            username: process.env.POSTGRES_USER,
-            entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: true,
-            ssl: {
-                /* <----- Add SSL option */
-                requestCert: true,
-                rejectUnauthorized: false,
-            },
-        }),
-        UsersModule,
-        AuthModule,
-        SubjectsModule,
-        PredictionModule
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..'),
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB,
+      port: parseInt(process.env.POSTGRES_PORT),
+      password: process.env.POSTGRES_PASSWORD,
+      username: process.env.POSTGRES_USER,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      ssl: {
+        /* <----- Add SSL option */
+        requestCert: true,
+        rejectUnauthorized: false,
+      },
+    }),
+    UsersModule,
+    AuthModule,
+    SubjectsModule,
+    PestDiseaseModule,
+    CropsTipsModule,
+    PredictionModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {
 }
